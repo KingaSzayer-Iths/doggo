6c2b23a8cd66e958ef3cbef418ec917b329feead
KingaSzayer-Iths <kinga.szayer@iths.se>
2026-08-09 11:22:59 +0200

    Changed from class to className in activites.tsx

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -50,7 +50,7 @@ function Activities() {
         <button className="location-button" type="button"
           onClick={() => setShowMap(!showMap)}
         >
-          <svg class="m-1 text-emerald-800" width="24" height="24" viewBox="0 0 24 24" fill="none"
+          <svg className="m-1 text-emerald-800" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
             aria-hidden="true">
             <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
